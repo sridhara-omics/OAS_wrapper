ca53d501e430ff25bfa37ff61cada3df62649bc5
Viswanadham Sridhara <sridhara.omics@gmail.com>
2024-06-06 09:16:36 +0530

    output of bdist_wheel included

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='A wrapper for Observed Antibody Space (OAS) datasets',
+    name='OAS_wrapper',
     version='0.1',
     packages=find_packages(),
     install_requires=[
