e71f8136cba3cda39cecac3c3184e401d1a07da7
Viswanadham Sridhara <sridhara.omics@gmail.com>
2024-11-25 12:23:30 +0530

    update tabulate query related build dist wheel

--- build/lib/OAS_wrapper/tabulate_query_sequence.py
+++ build/lib/OAS_wrapper/tabulate_query_sequence.py
@@ -1,55 +1,58 @@
 import pandas as pd
 
-def tabulate_query_sequence(query_sequence, data_unit_file, v_sequences_df, d_sequences_df, j_sequences_df):
-    """ Given a query sequence that is in the dataset, the output is a table that has relevant fields of interest picked from 200 columns of OAS dataset.
-    In addition, since the actual V, D and J sequences are absent, and only the identifiers are provided in OAS dataset, we use IMGT table and map the identifier to the sequence and provide the information in the result.
-
+def tabulate_query_sequence(query_sequence, data_unit_file, v_sequences_df, d_sequences_df, j_sequences_df, paired=True):
+    """
+    Given a query sequence that is in the dataset, the output is a table that has relevant fields of interest picked 
+    from 200 columns of the OAS dataset. In addition, since the actual V, D, and J sequences are absent, and only 
+    the identifiers are provided in the OAS dataset, we use the IMGT table and map the identifier to the sequence 
+    and provide the information in the result.
 
     Args:
-        query_sequence (_type_): query nucleotide sequence
-        df (_type_): path of the OAS dataset file
-        v_sequences_df (_type_): path of IGHV IMGT file
-        d_sequences_df (_type_): path of IGHD IMGT file
-        j_sequences_df (_type_): path of IGHJ IMGT file
+        query_sequence (str): query nucleotide sequence
+        data_unit_file (str): path of the OAS dataset file
+        v_sequences_df (DataFrame): DataFrame of IGHV IMGT file
+        d_sequences_df (DataFrame): DataFrame of IGHD IMGT file
+        j_sequences_df (DataFrame): DataFrame of IGHJ IMGT file
+        paired (bool): If True, '_heavy' suffix is included in column names. Otherwise, it is excluded.
 
     Returns:
-        _type_: List that has the information filtered out from a large subset of columns, along with V/D/J sequences mapped to original IMGT database.
+        dict: Dictionary with the filtered information and mapped sequences.
     """
-    # Find the row that matches the input query sequence
+    # Determine column suffix based on paired status
+    suffix = "_heavy" if paired else ""
+
+    # Read the data unit file
     df = pd.read_csv(data_unit_file, low_memory=False, skiprows=1)
-    matched_row = df[df['sequence_heavy'] == query_sequence].iloc[0]
+
+    # Find the row that matches the input query sequence
+    matched_row = df[df[f'sequence{suffix}'] == query_sequence].iloc[0]
     
     # Read relevant data from the matched row
-    v_call_heavy = matched_row['v_call_heavy']
-    d_call_heavy = matched_row['d_call_heavy']
-    j_call_heavy = matched_row['j_call_heavy']
-    sequence_alignment_heavy = matched_row['sequence_alignment_heavy']
-    germline_alignment_heavy = matched_row['germline_alignment_heavy']
+    v_call = matched_row[f'v_call{suffix}']
+    d_call = matched_row[f'd_call{suffix}']
+    j_call = matched_row[f'j_call{suffix}']
+    sequence_alignment = matched_row[f'sequence_alignment{suffix}']
+    germline_alignment = matched_row[f'germline_alignment{suffix}']
     
     # Map V, D, and J calls to their respective sequences
-    v_sequence = v_sequences_df.loc[v_sequences_df['v_call'] == v_call_heavy, 'sequence'].values[0]
-    d_sequence = d_sequences_df.loc[d_sequences_df['d_call'] == d_call_heavy, 'sequence'].values[0]
-    j_sequence = j_sequences_df.loc[j_sequences_df['j_call'] == j_call_heavy, 'sequence'].values[0]
+    v_sequence = v_sequences_df.loc[v_sequences_df['v_call'] == v_call, 'sequence'].values[0]
+    d_sequence = d_sequences_df.loc[d_sequences_df['d_call'] == d_call, 'sequence'].values[0]
+    j_sequence = j_sequences_df.loc[j_sequences_df['j_call'] == j_call, 'sequence'].values[0]
     
     # Return relevant fields
     return {
-        'sequence_heavy': query_sequence,
-        'v_call': v_call_heavy,
+        f'sequence{suffix}': query_sequence,
+        'v_call': v_call,
         'v_sequence': v_sequence,
-        'd_call': d_call_heavy,
+        'd_call': d_call,
         'd_sequence': d_sequence,
-        'j_call': j_call_heavy,
+        'j_call': j_call,
         'j_sequence': j_sequence,
-        'sequence_alignment_heavy': sequence_alignment_heavy,
-        'germline_alignment_heavy': germline_alignment_heavy
+        f'sequence_alignment{suffix}': sequence_alignment,
+        f'germline_alignment{suffix}': germline_alignment
     }
 
 # Example usage
-# Assuming you have loaded your DataFrame and sequences DataFrames (df, v_sequences_df, d_sequences_df, j_sequences_df)
 # query_sequence = 'TGGGGGACTCCTGTGCCCCACCATGGACACACTTTGCTCCACGCTCCTGCTGCTGATCATCCCTTCATGGGTCTTGTCCCAGATCACCTTGGAGGAGTCTGGTCCTACACTGGTGAAACCCACACAGACCCTCACGCTGACATGCACCTTTTCTGGGTTCTCACTCAGCACTAGTGGAGTGGGTGTGGGCTGGATCCGTCAGCCCCCAGGAAAGGCCCTGGAGTGGCTTGCACTCATTTATTGGGACGGTAATAACCTCTATAGTCCATCTCTGCAGAACAGGCTCACCATCACCAAGGACACCTCCAAAAACCAGGTGGTCCTTACAATGACCAACATGGACCCTATTGACACAGCCACATATTTCTGTGCTCACTTCTCTGCTACTGTGAGTCGGGGATTTATGAATATCAGGAGAAAAAAGCCCCAGAGTTATGACTACTGGGGCCAGGGAAGTCTGGTCACCGTCTCCTCAGCATCCCCGACCAGCCCCAAGGTCTTCCCGCTGAGCCTCTGCAGCACCCAGCCAGATGGGAACGTGGTCATCGCCTGCCTGGTCCAGGGCTTCTTCCCCCAGGAGCCACTCAGTGTGACCTGGAGCGAAAGCGGACAGGGCGTGACCGCCAGAAACTTCCC'
-# df = pd.read_csv("../data/1279050_1_Paired_All.csv.gz", low_memory=False, skiprows=1)
-# v_sequences_df = pd.read_csv("../refs/imgt_human_IGHV_sequences.txt", names=("v_call", "sequence"), header=None, sep="\t")
-# d_sequences_df = pd.read_csv("../refs/imgt_human_IGHD_sequences.txt", names=("d_call", "sequence"), header=None, sep="\t")
-# j_sequences_df = pd.read_csv("../refs/imgt_human_IGHJ_sequences.txt", names=("j_call", "sequence"), header=None, sep="\t")
-# result = tabulate_query_sequence(query_sequence, df, v_sequences_df, d_sequences_df, j_sequences_df)
+# result = tabulate_query_sequence(query_sequence, "../data/1279050_1_Paired_All.csv.gz", v_sequences_df, d_sequences_df, j_sequences_df, paired=True)
 # print(result)
